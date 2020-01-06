cf1443d9e7b459dd4ac448767ac7b11eb89caf24
Chad Jemmett <chad.jemmett@gmail.com>
2020-01-06 14:28:38 -0700

    More functional tests

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 home_page = None
 
-def home_page():
-    pass
+def home_page(request):
+    # return HttpResponse()
+    return HttpResponse('<html><title>To-Do</title></html>')
 # Create your views here.
